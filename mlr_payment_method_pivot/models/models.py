05ed6c258bca0840a2c23dcdf7774f28a5b6fef9
sagar <sagars209@gmail.com>
2024-04-04 11:37:11 +0530

    applied the changes to fix the pivot not showing payment method name

--- mlr_payment_method_pivot/models/models.py
+++ mlr_payment_method_pivot/models/models.py
@@ -8,11 +8,12 @@ from odoo import models, fields
 class PosOrderReport(models.Model):
     _inherit = "report.pos.order"
     payment_method_id = fields.Many2one(
-                'pos.payment', string='payment_method_id', readonly=True)
+                'pos.payment', string='Payment', readonly=True)
+    payment_id = fields.Many2one('pos.payment.method', string='payment_method_id')
 
     def _select(self):
         return super(PosOrderReport, self)._select() + """
-                ,pp.payment_method_id AS payment_method_id
+                ,pp.payment_method_id AS payment_id, pp.payment_method_id
         """
 
     def _from(self):
